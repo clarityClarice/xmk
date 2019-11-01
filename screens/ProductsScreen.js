33dd712f0ec5952ada382340161e2ce9f1f4939a
Cecília Clarice <51001467+clarityClarice@users.noreply.github.com>
2019-11-01 14:07:21 -0300

    Botões para navegação na aba produtos

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler'
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
 
-export default function ProductsScreen() {
-  return (
-    <View>
-      <Text>Produtos</Text>
-    </View>
-  )
+
+export default class ProductsScreen extends React.Component {
+
+  handleNavigateCatalogue = () => {
+    const { navigation } = this.props
+    navigation.navigate('Catalago')
+  }
+
+  render(){
+    return (
+      <ScrollView style={styles.Container}>
+        <View style={styles.buttonRow}>
+          <RectButton style={styles.pathButton} onPress={() => this.handleNavigateCatalogue()}>
+            <Ionicons name="md-bookmarks" color="#F5B0C2" size={50}/>
+            <Text style={styles.buttonText}>Catálago</Text>
+          </RectButton>
+          <RectButton style={styles.pathButton}>
+            <Ionicons name="md-clipboard" color="#F5B0C2" size={50}/>
+            <Text style={styles.buttonText}>Estoque</Text>
+          </RectButton>
+        </View>
+        <View style={styles.buttonRow}>
+          <RectButton style={styles.pathButton}>
+            <Ionicons name="md-star" color="#F5B0C2" size={50}/>
+            <Text style={styles.buttonText}>Favoritos</Text>
+          </RectButton>
+          <RectButton style={styles.pathButton}>
+            <Ionicons name="md-cart" color="#F5B0C2" size={50}/>
+            <Text style={styles.buttonText}>Pedidos</Text>
+          </RectButton>
+        </View>
+        <View style={styles.buttonRow} >
+          <RectButton style={styles.pathButton}>
+            <Ionicons name="md-search" color="#F5B0C2" size={50}/>
+            <Text style={styles.buttonText}>Buscar</Text>
+          </RectButton>
+        </View>
+      </ScrollView>
+    )
+  }
 }
 
 ProductsScreen.navigationOptions = {
@@ -15,4 +51,29 @@ ProductsScreen.navigationOptions = {
     backgroundColor: '#F5B0C2',
   },
   headerTintColor: '#fff',
-};
+}
+
+
+const styles = StyleSheet.create({
+  Container: {
+
+  },
+  buttonRow: {
+    flexDirection: "row",
+    padding: 20,
+    justifyContent: "space-around"
+  },
+  pathButton: {
+    width: 150,
+    height: 150,
+    borderRadius: 4,
+    backgroundColor: '#FFE8EB',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    marginTop: 5,
+    fontSize: 20
+  }
+})
